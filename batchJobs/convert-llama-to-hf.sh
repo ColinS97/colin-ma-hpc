13e647d324d762521ee91037c8df500eaff8154d
Colin Simon <s81601@htw-dresden.de>
2023-05-10 17:57:23 +0200

    download and convert llama

---
+++ batchJobs/convert-llama-to-hf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --no-requeue
+#SBATCH --partition=gpu2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32000
+#SBATCH --mincpus=1
+#SBATCH --time=03:00:00
+#SBATCH --job-name=convert-llama-model-to-hf
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
+#SBATCH --output=output-%j.out
+#SBATCH --error=error-%j.err
+
+module --force purge
+module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
+
+pip install transformers
+
+python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
+    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 7B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
+
+
+python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
+    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 13B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
+
+python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
+    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 30B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
+
