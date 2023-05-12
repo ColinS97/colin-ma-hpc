6567e3951cc517b00abd90a29e50cfb49fe97ac4
Colin Simon <s81601@htw-dresden.de>
2023-05-12 12:33:58 +0200

    update dsp server

--- batchJobs/run-dsp-server.sh
+++ batchJobs/run-dsp-server.sh
@@ -18,7 +18,7 @@ module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
 
 /sw/installed/Python/3.10.4-GCCcore-11.3.0/bin/python3.10 -m pip install --upgrade pip
 
-pip install transformers accelerate sentencepiece uvicorn git+https://github.com/ColinS97/dsp
+pip install transformers accelerate sentencepiece uvicorn fastapi git+https://github.com/ColinS97/dsp
 
 /home/cosi765e/colin-ma-scratch/tools/ngrok http 4242 --log=stdout > ngrok.log &
 
