e8a7fdba721967bfe03657b24e25ec320b278d5c
Colin Simon <s81601@htw-dresden.de>
2023-05-12 12:44:43 +0200

    try high ram partition

--- batchJobs/convert-llama-to-hf-high-ram.sh
+++ batchJobs/convert-llama-to-hf-high-ram.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --no-requeue
-#SBATCH --partition=gpu2
+#SBATCH --partition=haswell
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32000
+#SBATCH --mem=64000
 #SBATCH --mincpus=1
 #SBATCH --time=01:30:00
 #SBATCH --job-name=convert-llama-model-to-hf
@@ -19,14 +19,6 @@ module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
 /sw/installed/Python/3.10.4-GCCcore-11.3.0/bin/python3.10 -m pip install --upgrade pip
 
 pip install transformers accelerate sentencepiece
-
-
-#python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
-#    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 7B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
-
-
-#python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
-#    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 13B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/13B
 
 python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
     --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 30B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/30B
