da5f5debf5b87698f75f72235655d418e79cbff5
Colin Simon <s81601@htw-dresden.de>
2023-05-12 12:15:38 +0200

    run dsp

--- batchJobs/run-dsp-server.sh
+++ batchJobs/run-dsp-server.sh
@@ -4,30 +4,22 @@
 #SBATCH --partition=gpu2
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32000
+#SBATCH --mem=4000
 #SBATCH --mincpus=1
 #SBATCH --time=03:00:00
-#SBATCH --job-name=convert-llama-model-to-hf
+#SBATCH --job-name=run-dsp-server
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
-#SBATCH --output=output-%j.out
-#SBATCH --error=error-%j.err
+#SBATCH --output=output-run-dsp-server-%j.out
+#SBATCH --error=output-run-dsp-server-%j.out
 
 module --force purge
 module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
 
 /sw/installed/Python/3.10.4-GCCcore-11.3.0/bin/python3.10 -m pip install --upgrade pip
 
-pip install transformers accelerate sentencepiece 
+pip install transformers accelerate sentencepiece git+https://github.com/ColinS97/dsp
 
+/home/cosi765e/colin-ma-scratch/tools/ngrok http 4242 --log=stdout > ngrok.log &
 
-python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
-    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 7B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
-
-
-python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
-    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 13B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/13B
-
-#python /home/cosi765e/transformers/src/transformers/models/llama/convert_llama_weights_to_hf.py \
-#    --input_dir /home/cosi765e/colin-ma-scratch/models/llama --model_size 30B --output_dir /home/cosi765e/colin-ma-scratch/models/hf-llama/7B
-
+python -m dsp.modules.hf_server --port 4242 --model "google/flan-t5-base"
