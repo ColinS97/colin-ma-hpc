c6b00daf5719266be7ffb4e14e28fa592090c61b
Colin Simon <s81601@htw-dresden.de>
2023-05-17 16:30:10 +0200

    add echo to ngrok_url

--- batchJobs/run-dsp-server.sh
+++ batchJobs/run-dsp-server.sh
@@ -27,6 +27,7 @@ module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
 pip install transformers accelerate sentencepiece uvicorn fastapi 
 pip install --no-cache-dir git+https://github.com/ColinS97/dsp
 /scratch/ws/0/cosi765e-colin-ma-scratch/tools/ngrok http 4242 --log=stdout > ngrok.log &
-grep -Eoh 'https://.*\.ngrok-free\.app' ngrok.log
+NGROK_URL=$(grep -Eoh 'https://.*\.ngrok-free\.app' ngrok.log)
+echo "$NGROK_URL"
 
 python -m dsp.modules.hf_server --port 4242 --model "$model"
