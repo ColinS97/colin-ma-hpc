7ca5a2f7076e4c532a99c4b7aee61e5d23b5e8a5
cosi765e <cosi765e@tauruslogin5.taurus.hrsk.tu-dresden.de>
2023-05-24 16:46:55 +0200

    fix fschat server

--- batchJobs/run-fschat-openai-server.sh
+++ batchJobs/run-fschat-openai-server.sh
@@ -11,20 +11,12 @@
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
 #SBATCH --output=output-run-fschat-openai-server.out
 
-if [ "$#" -ne 1 ]
-then
-    echo "Please specify a model name as argument. e.g. h2oai/h2ogpt-oig-oasst1-512-6.9b or h2oai/h2ogpt-oig-oasst1-512-6.9b"
-    exit 1
-fi
-model=$1
-
 module --force purge
 module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
 
 /sw/installed/Python/3.10.4-GCCcore-11.3.0/bin/python3.10 -m pip install --upgrade pip
 
 pip install transformers accelerate sentencepiece uvicorn fastapi fschat
-pip install --no-cache-dir git+https://github.com/ColinS97/dsp
 
 nohup python3 -m fastchat.serve.controller &
 
@@ -34,4 +26,4 @@ nohup python3 -m fastchat.serve.model_worker --model-name 'vicuna-13b-v1.1' --mo
 NGROK_URL=$(grep -Eoh 'https://.*\.ngrok-free\.app' ngrok.log)
 echo "$NGROK_URL"
 
-nohup python3 -m fastchat.serve.openai_api_server --host localhost --port 8000 &
+python3 -m fastchat.serve.openai_api_server --host localhost --port 8000
