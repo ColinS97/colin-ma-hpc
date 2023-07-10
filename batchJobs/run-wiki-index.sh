c328a1c4cef819ee9549d6b691b2a405fdf36433
Colin Simon <s81601@htw-dresden.de>
2023-07-10 10:38:37 +0200

    wiki index

---
+++ batchJobs/run-wiki-index.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --no-requeue
+#SBATCH --partition=gpu2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000
+#SBATCH --mincpus=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=run-dsp-server
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
+#SBATCH --output=output-run-dsp-server.out
+
+
+module --force purge
+module load modenv/hiera  GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.12.0-CUDA-11.7.0
+
+/sw/installed/Python/3.10.4-GCCcore-11.3.0/bin/python3.10 -m pip install --upgrade pip
+
+pip install transformers chromadb
+
+python index_wikipedia.py
