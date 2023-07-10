54c61d794ac5c6be5065392a98f8b6c9258564a4
Colin Simon <s81601@htw-dresden.de>
2023-07-10 15:15:29 +0200

    packages

--- batchJobs/run-llama-server.sh
+++ batchJobs/run-llama-server.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=8000
 #SBATCH --mincpus=1
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 #SBATCH --job-name=run-llama-server-%j
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
@@ -24,6 +24,6 @@ source "/lustre/ssd/ws/cosi765e-colin-ma-ssd/llama-cpp-venv/pyenv/bin/activate"
 python -m pip install --upgrade pip
 
 CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python
-pip install uvicorn
+pip install uvicorn anyio
 
 python -m llama_cpp.server --model "/lustre/ssd/ws/cosi765e-colin-ma-ssd/vicuna-13b-v1.3.0.ggmlv3.q8_0.bin" --n_gpu_layers 80 --n_threads 1
