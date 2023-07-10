d5fcad7b0269222582986981886b6f7dd284ab3a
Colin Simon <s81601@htw-dresden.de>
2023-07-10 14:19:30 +0200

    llama-server

---
+++ batchJobs/run-llama-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --no-requeue
+#SBATCH --partition=gpu2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000
+#SBATCH --mincpus=1
+#SBATCH --time=08:00:00
+#SBATCH --job-name=run-llama-server-%j
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=colin.simon@mailbox.tu-dresden.de
+#SBATCH --output=output-run-llama-server-%j.out
+# shellcheck source=/dev/null
+
+
+module --force purge
+module load modenv/hiera  modenv/scs5 CUDA/11.8.0
+
+virtualenv /lustre/ssd/ws/cosi765e-colin-ma-ssd/llama-cpp-venv/pyenv
+source "/lustre/ssd/ws/cosi765e-colin-ma-ssd/llama-cpp-venv/pyenv/bin/activate"
+
+
+python -m pip install --upgrade pip
+
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python
+
+python -m llama_cpp.server --model "/lustre/ssd/ws/cosi765e-colin-ma-ssd/vicuna-13b-v1.3.0.ggmlv3.q8_0.bin" --n_gpu_layers 80 --n_threads 1
